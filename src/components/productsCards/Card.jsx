d4bdd289c1b4ded61b4f145d53c39807319185a5
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-04-17 09:11:32 -0300

    card modified

--- src/components/productsCards/Card.jsx
+++ src/components/productsCards/Card.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import prueba from '../../assets/images/imagen-prueba.jpg'
-import { ButtonLink } from '../ButtonLink'
 
 export const Card = () => {
   return (
-      <div className="flex flex-col items-center justify-center gap-4 w-full sm:w-52 bg-gradient-to-t from-primary-200 p-4 shadow-lg">
-          <figure className='w-24 rounded-full ring ring-primary-100 ring-offset-2'>
-            <img src={prueba} alt="Shoes" loading='lazy' className='rounded-full'/>
+      <div className="flex flex-col rounded items-center justify-center gap-2 w-36 sm:w-60 shadow-lg">
+          <figure className='w-full rounded-t'>
+            <img src={prueba} alt="Shoes" loading='lazy' className='object-cover rounded-t'/>
           </figure>
-          <div className="w-full flex flex-col items-center justify-center">
-              <div className='text-gray-700 text-center'>
-                <h2 className='font-bold text-2xl'>Shoes!</h2>
-                <p className='font-semibold text-xl'>$99,99</p>
+          <div className="w-full flex flex-col items-center justify-center p-1">
+              <div className='text-gray-700 w-full text-start'>
+                <p className='font-semibold text-md'>$99,99</p>
+                <h2 className=' text-sm'>Shoes!</h2>
               </div>
-              <div className="mt-4">
-                <ButtonLink content={
-                    <>
-                        <span>Encargar</span> <i className="fa-brands fa-whatsapp text-social-300"></i>
-                    </>
-                }/>
+              <div className="mt-1">
+                <button 
+                className='flex items-center justify-center text-primary-100
+                gap-2 p-2 rounded duration-200 hover:bg-primary-150 hover:text-gray-600'>
+                  <span >Encargar</span> <i className="fa-brands fa-whatsapp text-social-300"></i>
+                </button>
               </div>
           </div>
       </div>
--- src/components/productsCards/ProductsCards.jsx
+++ src/components/productsCards/ProductsCards.jsx
@@ -4,8 +4,10 @@ import { Card } from './Card'
 export const ProductsCards = () => {
   return (
     <section className='flex w-full flex-col items-center justify-center gap-2 p-2'>
-            <form>
-                <input type="text" />    
+            <form>         
+              <input 
+              type="text" 
+              placeholder="Buscar Productos" className="input bg-white border-1 border-gray-400 w-full max-w-xs text-gray-600" />
             </form>
             <article className='w-full flex gap-2 flex-wrap items-center justify-center'>
                 <Card />
