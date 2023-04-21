ba91a25c7fcec168426f003a9d2afa83ad06620c
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-04-21 18:20:09 -0300

    link fixed

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -4,7 +4,7 @@ export const ButtonLink = ({content}) => {
   return (
     <a
       className="w-fit inline-block rounded border border-primary-100 bg-primary-100 px-6 py-2 md:px-12 md:py-3 text-sm font-medium text-white hover:bg-transparent hover:text-primary-100 focus:outline-none focus:ring active:text-primary-100"
-      href="/download"
+      href="/#productos"
     >
       {content}
     </a>
